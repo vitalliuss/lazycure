52ad0690cebf7f4fd2fca497308518f1a224bc78
Vitali Shulha <vitalliuss@gmail.com>
2012-02-04 15:22:32 +0300

    Digital clock widget was added to main layout.

--- gen/com/github/lazycure/R.java
+++ gen/com/github/lazycure/R.java
@@ -10,14 +10,23 @@ package com.github.lazycure;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int white=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int digitalClock_textSize=0x7f050000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static final int digitalClock1=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f060000;
     }
 }
