72279906c6d23f5c04c1e940a0613f93eab2de03
Vitali Shulha <vitalliuss@gmail.com>
2012-02-04 23:22:05 +0300

    Edit field for activity was created. Button "Done!" was created.

--- gen/com/github/lazycure/R.java
+++ gen/com/github/lazycure/R.java
@@ -21,12 +21,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f070002;
         public static final int digitalClock1=0x7f070000;
+        public static final int editText1=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
+        public static final int button1_text=0x7f060001;
+        public static final int editText1_hint=0x7f060002;
     }
 }
