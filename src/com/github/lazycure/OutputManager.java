6db0c5351d6d2d426e6ce60c4dc9b478af2eb4d7
Vitali Shulha <vitalliuss@gmail.com>
2012-07-22 16:21:33 +0300

    Activities grouped by finish day. Closing issue #9

--- src/com/github/lazycure/OutputManager.java
+++ src/com/github/lazycure/OutputManager.java
@@ -1,6 +1,7 @@
 package com.github.lazycure;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
@@ -15,26 +16,44 @@ public class OutputManager {
 	public final static String ACTIVITY_PREFIX = "	";
 	public final static String DELIMITER = " - ";
 	public final static String TEST_ACTIVITY_NAME = "test";
+	public final static String DAY_SEPARATION_LINE = "=====";
+	public final static SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	public static int lastDayNumber;
 	
 	public static String FormatActivitiesList(List<Activity> activities){
 		StringBuffer buffer = new StringBuffer();
 		String activitiesList = EMPTY_STRING;
-		
-		SimpleDateFormat dateFormat = new SimpleDateFormat ("HH:mm:ss");
 		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-		for (Activity activity:activities) {
-			buffer.append(ACTIVITY_PREFIX);
-			buffer.append(activity.getName());
-			buffer.append(DELIMITER);
-			Date delta = activity.getDuration();
-			Log.d("Date" , "Inserting [" + activity.getName() + "] with duration: " + dateFormat.format(delta));
-			buffer.append(dateFormat.format(delta));
+
+		for (int i=0; i<activities.size(); i++) {
+			buffer.append(FormatActivityString(activities.get(i)));
 			buffer.append("\n");
+			if (i<activities.size()-1){
+				if (getFinishDayNumber(activities.get(i)) != getFinishDayNumber(activities.get(i+1))){
+					buffer.append(ACTIVITY_PREFIX + DAY_SEPARATION_LINE);
+					Date previousActivityDate = activities.get(i).getFinishTime();
+					buffer.append(Time.GetYYYYMMDD(previousActivityDate));
+					buffer.append(DAY_SEPARATION_LINE);
+					buffer.append("\n");
+				}
+			}
 		}
 		activitiesList = buffer.toString();
 		return activitiesList;
 	}
-	
+
+	public static String FormatActivityString(Activity activity){
+		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(ACTIVITY_PREFIX);
+		buffer.append(activity.getName());
+		buffer.append(DELIMITER);
+		Date delta = activity.getDuration();
+		Log.d("Date" , "Inserting [" + activity.getName() + "] with finish time: " + activity.getFinishTime().toString());
+		buffer.append(dateFormat.format(delta));
+		return buffer.toString();
+	}
+
 	public static List<Activity> UpdateActivitiesWithStartTime(List<Activity> activities){
         //Update missing start time records for all activities
 		for (int i=1;i<activities.size();i++){
@@ -43,5 +62,11 @@ public class OutputManager {
 			}
 		}
 		return activities;
+	}
+
+	public static int getFinishDayNumber(Activity activity){
+		int dayNumber = activity.getFinishTime().getDay();
+		Log.d("Date", "Day of week number: " + dayNumber);
+		return dayNumber;
 	}
 }
