6651617f6052e41d7f40c35524c84b1cc669ebc6
Vitali Shulha <vitalliuss@gmail.com>
2012-08-12 18:14:44 +0300

    "About" menu created

--- gen/com/github/lazycure/R.java
+++ gen/com/github/lazycure/R.java
@@ -11,29 +11,46 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040001;
-        public static final int white=0x7f040000;
+        public static final int black=0x7f050001;
+        public static final int white=0x7f050000;
     }
     public static final class dimen {
-        public static final int timeLabelTextSize=0x7f050000;
+        public static final int timeLabelTextSize=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon_about=0x7f020001;
+        public static final int icon_export=0x7f020002;
+        public static final int icon_settings=0x7f020003;
     }
     public static final class id {
-        public static final int activities_list_text=0x7f070004;
-        public static final int activities_title=0x7f070003;
-        public static final int done_button=0x7f070002;
-        public static final int input=0x7f070000;
-        public static final int timeLabel=0x7f070001;
+        public static final int about=0x7f090009;
+        public static final int aboutText=0x7f090001;
+        public static final int activities_list_text=0x7f090006;
+        public static final int activities_title=0x7f090005;
+        public static final int backButton=0x7f090000;
+        public static final int done_button=0x7f090004;
+        public static final int export=0x7f090007;
+        public static final int input=0x7f090002;
+        public static final int settings=0x7f090008;
+        public static final int timeLabel=0x7f090003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int main=0x7f030001;
+    }
+    public static final class menu {
+        public static final int menu=0x7f080000;
+    }
+    public static final class raw {
+        public static final int about=0x7f040000;
     }
     public static final class string {
-        public static final int activities=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int doneButton_text=0x7f060002;
-        public static final int input_hint=0x7f060003;
+        public static final int activities=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int backButton_text=0x7f070003;
+        public static final int doneButton_text=0x7f070002;
+        public static final int input_hint=0x7f070004;
+        public static final int title_about=0x7f070005;
     }
 }
